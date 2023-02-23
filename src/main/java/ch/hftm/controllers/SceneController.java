112894e787dda8167f5e2fa22bb549ac7b753815
leonardo.lang <leonardo.lang@hftm.ch>
2023-02-23 20:37:42 +0100

    Add changeScene method
    Because the AlbumController will not use an Event to change scenes the
    current changeScene method can't be used. A new changeScene method with
    a Scene instead of an Event as a parameter solves this.

--- src/main/java/ch/hftm/controllers/SceneController.java
+++ src/main/java/ch/hftm/controllers/SceneController.java
@@ -2,6 +2,7 @@ package ch.hftm.controllers;
 
 import ch.hftm.App;
 import ch.hftm.data.Album;
+import ch.hftm.data.Image;
 import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -28,6 +29,22 @@ public class SceneController {
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.show();
+    }
+
+    public <T> void changeScene(Scene scene, String sceneName, T data) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(String.format("view/%s.fxml", sceneName)));
+        root = fxmlLoader.load();
+
+        var controller = fxmlLoader.getController();
+        if (controller instanceof AlbumController && data instanceof ch.hftm.data.Album) {
+            ((AlbumController) controller).setAlbum((Album) data);
+        } else if (controller instanceof ImageController && data instanceof ch.hftm.data.Image) {
+            ((ImageController) controller).setImage((Image) data);
+        }
+
+        stage = (Stage) scene.getWindow();
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
