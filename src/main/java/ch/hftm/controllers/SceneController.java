864c75d1e4ebf38ea9160402fabe40a985432268
Joshua Lehmann <joshua.le1999@gmail.com>
2023-02-24 16:12:21 +0100

    #12 Improved the changeScene method to only one method used

--- src/main/java/ch/hftm/controllers/SceneController.java
+++ src/main/java/ch/hftm/controllers/SceneController.java
@@ -1,48 +1,29 @@
 package ch.hftm.controllers;
 
-import java.io.IOException;
-
 import ch.hftm.App;
 import ch.hftm.data.Album;
-import javafx.event.Event;
+import ch.hftm.data.Image;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class SceneController {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    public <T> void changeScene(Event event, String sceneName, T data) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(String.format("view/%s.fxml", sceneName)));
-        root = fxmlLoader.load();
-
-        var controller = fxmlLoader.getController();
-        if (controller instanceof AlbumController && data instanceof ch.hftm.data.Album) {
-            ((AlbumController) controller).setAlbum((Album) data);
-        }
-
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public <T> void changeScene(Scene scene, String sceneName, T data) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(String.format("view/%s.fxml", sceneName)));
-        root = fxmlLoader.load();
+        Parent root = fxmlLoader.load();
 
         var controller = fxmlLoader.getController();
-        if (controller instanceof AlbumController albumController && data instanceof ch.hftm.data.Album album) {
-            albumController.setAlbum(album);
-        } else if (controller instanceof ImageController imageController && data instanceof ch.hftm.data.Image image) {
-            imageController.setImage(image);
+        switch (controller) {
+            case AlbumController albumController -> albumController.setAlbum((Album) data);
+            case ImageController imageController -> imageController.setImage((Image) data);
+            default -> throw new IllegalStateException("Unexpected controller: " + controller.getClass().getName());
         }
 
-        stage = (Stage) scene.getWindow();
+        Stage stage = (Stage) scene.getWindow();
         stage.setScene(new Scene(root));
         stage.show();
     }
