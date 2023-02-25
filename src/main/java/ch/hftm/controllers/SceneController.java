3180797036dbf1efeed4ab34cfe40028cbc8ef6a
leonardo.lang <leonardo.lang@hftm.ch>
2023-02-25 08:33:02 +0100

    Refactor switch to if-else statement
    The switch statement would only work with the preview feature but
    VSCode doesn't support it correctly.

--- src/main/java/ch/hftm/controllers/SceneController.java
+++ src/main/java/ch/hftm/controllers/SceneController.java
@@ -1,14 +1,12 @@
 package ch.hftm.controllers;
 
+import java.io.IOException;
+
 import ch.hftm.App;
-import ch.hftm.data.Album;
-import ch.hftm.data.Image;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class SceneController {
 
@@ -17,10 +15,10 @@ public class SceneController {
         Parent root = fxmlLoader.load();
 
         var controller = fxmlLoader.getController();
-        switch (controller) {
-            case AlbumController albumController -> albumController.setAlbum((Album) data);
-            case ImageController imageController -> imageController.setImage((Image) data);
-            default -> throw new IllegalStateException("Unexpected controller: " + controller.getClass().getName());
+        if (controller instanceof AlbumController albumController && data instanceof ch.hftm.data.Album album) {
+            albumController.setAlbum(album);
+        } else if (controller instanceof ImageController imageController && data instanceof ch.hftm.data.Image image) {
+            imageController.setImage(image);
         }
 
         Stage stage = (Stage) scene.getWindow();
